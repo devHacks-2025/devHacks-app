f1d823de88378d4385775a0396fa0fa9b83af45d
JordonH03 <jordonhong03@gmail.com>
2025-02-20 00:01:46 -0600

    fix: Fix effect hook to render the camera properly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,34 +5,31 @@ import { SegmentedControl } from '@radix-ui/themes'
 import QrScanner from 'qr-scanner'
 
 export default function Home () {
-  const videoRef = useRef<HTMLVideoElement>(null)
-
   useEffect(() => {
-    const videoElem = videoRef.current
-    if (videoElem) {
-      const scanner = new QrScanner(
-        videoElem,
-        result => console.log('decoded qr code:', result),
-        { returnDetailedScanResult: true, }
-      )
-
-      scanner.start()
-      console.log(videoElem);
-    }
+    window.addEventListener('load', () => {
+      const video = document.getElementById('scanner') as HTMLVideoElement
+      if (video) {
+        const scanner = new QrScanner(
+          video,
+          result => console.log('decoded qr code:', result),
+          { returnDetailedScanResult: true, }
+        )
+        scanner.start()
+      }
+    })
   }, [])
 
   return (
       <main className="flex flex-col gap-8 row-start-2 items-center">
-        <div className="toast-container position-fixed bottom-0 start-50 translate-middle-x p-3" id="toastContainer">
-        </div>
+        <video id='scanner' disablePictureInPicture></video>
 
         <SegmentedControl.Root defaultValue="checkin" size='3' >
           <SegmentedControl.Item value="checkin">Check-in</SegmentedControl.Item>
           <SegmentedControl.Item value="verify">Verify</SegmentedControl.Item>
         </SegmentedControl.Root>
 
-        <div id="reader" className="container-xl align-items-stretch"></div>
-        <video id='scanner' disablePictureInPicture></video>
+        <div className="toast-container position-fixed bottom-0 start-50 translate-middle-x p-3" id="toastContainer">
+        </div>
       </main>
   )
 }
